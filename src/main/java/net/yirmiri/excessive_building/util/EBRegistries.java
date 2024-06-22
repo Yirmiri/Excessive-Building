eb9eb5bbc77e2557e251e9fa0d5f63e786e3c280
yirmiri <yirmirilucy@gmail.com>
2024-06-22 07:07:29 -0400

    Flammables & Fuels

--- src/main/java/net/yirmiri/excessive_building/util/EBRegistries.java
+++ src/main/java/net/yirmiri/excessive_building/util/EBRegistries.java
@@ -12,11 +12,7 @@ public class EBRegistries {
         registerStrippables();
         registerCompostables();
         registerFuels();
-
-        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC, 20, 5);
-        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_STAIRS, 20, 5);
-        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_SLAB, 20, 5);
-        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_VERTICAL_STAIRS, 20, 5);
+        registerFlammables();
     }
 
     public static void registerStrippables() {
@@ -25,10 +21,111 @@ public class EBRegistries {
     }
 
     public static void registerFuels() {
+        FuelRegistry.INSTANCE.add(EBBlocks.OAK_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.SPRUCE_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.BIRCH_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.JUNGLE_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.ACACIA_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.DARK_OAK_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.MANGROVE_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.CHERRY_VERTICAL_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.BAMBOO_VERTICAL_STAIRS, 150);
+
         FuelRegistry.INSTANCE.add(EBBlocks.OAK_MOSAIC, 150);
         FuelRegistry.INSTANCE.add(EBBlocks.OAK_MOSAIC_STAIRS, 150);
         FuelRegistry.INSTANCE.add(EBBlocks.OAK_MOSAIC_SLAB, 150);
         FuelRegistry.INSTANCE.add(EBBlocks.OAK_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.SPRUCE_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.SPRUCE_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.SPRUCE_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.SPRUCE_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.BIRCH_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.BIRCH_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.BIRCH_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.BIRCH_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.ACACIA_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.ACACIA_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.ACACIA_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.ACACIA_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.DARK_OAK_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.DARK_OAK_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.DARK_OAK_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.DARK_OAK_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.MANGROVE_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.MANGROVE_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.MANGROVE_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.MANGROVE_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.CHERRY_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.CHERRY_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.CHERRY_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.CHERRY_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.JUNGLE_MOSAIC, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.JUNGLE_MOSAIC_STAIRS, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.JUNGLE_MOSAIC_SLAB, 150);
+        FuelRegistry.INSTANCE.add(EBBlocks.JUNGLE_MOSAIC_VERTICAL_STAIRS, 150);
+
+        FuelRegistry.INSTANCE.add(EBBlocks.BAMBOO_MOSAIC_VERTICAL_STAIRS, 150);
+    }
+
+    public static void registerFlammables() {
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.SPRUCE_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BIRCH_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.JUNGLE_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.ACACIA_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.DARK_OAK_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.MANGROVE_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.CHERRY_VERTICAL_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BAMBOO_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.OAK_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.SPRUCE_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.SPRUCE_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.SPRUCE_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.SPRUCE_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BIRCH_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BIRCH_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BIRCH_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BIRCH_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.JUNGLE_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.JUNGLE_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.JUNGLE_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.JUNGLE_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.ACACIA_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.ACACIA_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.ACACIA_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.ACACIA_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.DARK_OAK_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.DARK_OAK_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.DARK_OAK_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.DARK_OAK_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.MANGROVE_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.MANGROVE_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.MANGROVE_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.MANGROVE_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.CHERRY_MOSAIC, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.CHERRY_MOSAIC_STAIRS, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.CHERRY_MOSAIC_SLAB, 20, 5);
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.CHERRY_MOSAIC_VERTICAL_STAIRS, 20, 5);
+
+        FlammableBlockRegistry.getDefaultInstance().add(EBBlocks.BAMBOO_MOSAIC_VERTICAL_STAIRS, 20, 5);
     }
 
     private static void registerCompostables() {
