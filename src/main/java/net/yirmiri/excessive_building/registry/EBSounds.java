9b6d4d48d04a951e6071b766d0aa2af42bfef741
yirmiri <yirmirilucy@gmail.com>
2024-07-07 19:48:39 -0400

    Fixed sounds + increased volume

--- src/main/java/net/yirmiri/excessive_building/registry/EBSounds.java
+++ src/main/java/net/yirmiri/excessive_building/registry/EBSounds.java
@@ -9,18 +9,18 @@ import net.yirmiri.excessive_building.ExcessiveBuilding;
 
 public class EBSounds {
     //COBBLED BRICKS
-    public static final SoundEvent COBBLED_BRICKS_BREAK = register("cobbled_bricks_break");
-    public static final SoundEvent COBBLED_BRICKS_STEP = register("cobbled_bricks_step");
-    public static final SoundEvent COBBLED_BRICKS_PLACE = register("cobbled_bricks_place");
-    public static final SoundEvent COBBLED_BRICKS_HIT = register("cobbled_bricks_hit");
-    public static final SoundEvent COBBLED_BRICKS_FALL = register("cobbled_bricks_fall");
+    public static final SoundEvent COBBLED_BRICKS_BREAK = register("block.cobbled_bricks.break");
+    public static final SoundEvent COBBLED_BRICKS_STEP = register("block.cobbled_bricks.step");
+    public static final SoundEvent COBBLED_BRICKS_PLACE = register("block.cobbled_bricks.place");
+    public static final SoundEvent COBBLED_BRICKS_HIT = register("block.cobbled_bricks.hit");
+    public static final SoundEvent COBBLED_BRICKS_FALL = register("block.cobbled_bricks.fall");
 
     //GROUPS
-    public static BlockSoundGroup COBBLED_BRICKS = new BlockSoundGroup(1F, 1F,
-            COBBLED_BRICKS_HIT, COBBLED_BRICKS_PLACE, COBBLED_BRICKS_PLACE, COBBLED_BRICKS_HIT, COBBLED_BRICKS_HIT);
+    public static BlockSoundGroup COBBLED_BRICKS = new BlockSoundGroup(1.0F, 1.0F,
+            COBBLED_BRICKS_BREAK, COBBLED_BRICKS_STEP, COBBLED_BRICKS_PLACE, COBBLED_BRICKS_HIT, COBBLED_BRICKS_FALL);
 
     private static SoundEvent register(String id) {
-        return Registry.register(Registries.SOUND_EVENT, Identifier.of(ExcessiveBuilding.MOD_ID), SoundEvent.of(Identifier.of(ExcessiveBuilding.MOD_ID)));
+        return Registry.register(Registries.SOUND_EVENT, Identifier.of(ExcessiveBuilding.MOD_ID, id), SoundEvent.of(Identifier.of(ExcessiveBuilding.MOD_ID, id)));
     }
 
     public static void registerEBSounds() {
