fd6af61fef291b505c1284ad0da199bb5098c758
yirmiri <yirmirilucy@gmail.com>
2024-11-27 20:55:30 -0500

    Let's do this one more time...

--- common/src/main/java/net/yirmiri/excessive_building/ExcessiveBuilding.java
+++ common/src/main/java/net/yirmiri/excessive_building/ExcessiveBuilding.java
@@ -1,24 +1,14 @@
 package net.yirmiri.excessive_building;
 
-import net.minecraft.resources.ResourceLocation;
-import net.yirmiri.excessive_building.registry.EBBlocks;
-import net.yirmiri.excessive_building.registry.EBItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExcessiveBuilding {
-
     public static final String MOD_ID = "excessive_building";
     public static final String MOD_NAME = "Excessive Building";
-    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
-        EBItems.register();
-        EBBlocks.register();
-    }
 
-    public static ResourceLocation id(String name) {
-        return new ResourceLocation(MOD_ID, name);
     }
 }
--- common/src/main/java/net/yirmiri/excessive_building/block/EBHalfTransparentBlock.java
+++
@@ -1,9 +0,0 @@
-package net.yirmiri.excessive_building.block;
-
-import net.minecraft.world.level.block.HalfTransparentBlock;
-
-public class EBHalfTransparentBlock extends HalfTransparentBlock {
-    public EBHalfTransparentBlock(Properties $$0) {
-        super($$0);
-    }
-}
