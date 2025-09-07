4986bff29c3b5bd3f0f0911d873475df14bc9457
Yirmiri <68178497+Yirmiri@users.noreply.github.com>
2025-09-07 04:16:59 -0400

    Fix boats

--- common/src/main/java/net/yirmiri/excessive_building/core/mixin/BoatMixin.java
+++ common/src/main/java/net/yirmiri/excessive_building/core/mixin/BoatMixin.java
@@ -2,7 +2,6 @@ package net.yirmiri.excessive_building.core.mixin;
 
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.yirmiri.excessive_building.common.util.EBUtil;
 import net.yirmiri.excessive_building.core.registry.EBItems;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Boat.class)
 public abstract class BoatMixin {
-//    @Shadow public abstract Object getVariant();
+    @Shadow public abstract Boat.Type getVariant();
 
-//    @Inject(method = "getDropItem", at = @At(value = "RETURN"), cancellable = true)
-//    private void excessiveBuilding$getDropItem(CallbackInfoReturnable<Item> cir) {
-//        if (this.getVariant() == EBUtil.ANCIENT_BOAT) {
-//            cir.setReturnValue(EBItems.ANCIENT_BOAT.get());
-//        }
-//    }
+    @Inject(at = @At(value = "RETURN"), method = "getDropItem", cancellable = true)
+    public void excessiveBuilding$getDropItem(CallbackInfoReturnable<Item> cir) {
+        if (this.getVariant() == EBUtil.ANCIENT_BOAT) {
+            cir.setReturnValue(EBItems.ANCIENT_BOAT.get());
+        }
+    }
 }
